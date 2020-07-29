392d863ae00baebe66ff79ee20f5144b6bdedd61
Katherine Anne Matton <katiemat@armis2-login1.arc-ts.umich.edu>
2020-07-29 13:55:34 -0400

    edits to score transcriptions arguments

--- evaluation/score_transcriptions.sh
+++ evaluation/score_transcriptions.sh
@@ -2,7 +2,7 @@
 kaldi_dir=$1
 ref_file_path=$2
 asr_file_path=$3
-output_dir=$4
+output_file_path=$4
 cd $1
 . ./path.sh
-compute-wer --text --mode=present ark:$2 ark:$3/ > $4/WER_output.txt
+compute-wer --text --mode=present ark:$2 ark:$3 > $4
